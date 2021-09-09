daea01cf8e6cd5b9920ee039ebcb90b1381c2300
Tiago Costa <88850683+T1aguinh0@users.noreply.github.com>
2021-09-09 17:18:30 -0300

    Create exercicio01.py

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
+n3 = int(input("Digite o terceiro número inteiro: "))
+
+listanumeros = [n1,n2,n3]
+print ("Lista dos números somados",listanumeros)
+
+print ("Total =",sum(listanumeros))

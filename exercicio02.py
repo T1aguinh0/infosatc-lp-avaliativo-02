77bece7a7aa4e296cdfc766649107c57ab88735c
Tiago Costa <88850683+T1aguinh0@users.noreply.github.com>
2021-09-09 17:18:36 -0300

    Create exercicio02.py

---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+item1 = input("Digite algo para sua lista: ")
+item2 = input("Digite algo para sua lista: ")
+item3 = input("Digite algo para sua lista: ")
+lista = [item1,item2,item3]
+
+nova_lista = lista.copy()
+print(nova_lista)

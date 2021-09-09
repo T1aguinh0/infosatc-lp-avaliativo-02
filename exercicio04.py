32646fa9560f1a6d26aa89344384a57306be05b9
Tiago Costa <88850683+T1aguinh0@users.noreply.github.com>
2021-09-09 17:18:48 -0300

    Create exercicio04.py

---
+++ exercicio04.py
@@ -0,0 +1,9 @@
+item1 = input("Adicione um item na lista: ")
+item2 = input("Adicione um item na lista: ")
+item3 = input("Adicione um item na lista: ")
+
+lista = [item1, item2, item3]
+
+verificar = input("Deseja verificar a posição de qual item? ")
+
+print("Está na posição",lista.index(verificar))

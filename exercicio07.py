853be4968a255ac5e79ddd452db3dd8299352bc5
Tiago Costa <88850683+T1aguinh0@users.noreply.github.com>
2021-09-09 17:19:25 -0300

    Create exercicio07.py

---
+++ exercicio07.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
+n3 = int(input("Digite o terceiro número inteiro: "))
+n4 = int(input("Digite o quarto número inteiro: "))
+n5 = int(input("Digite o quinto número inteiro: "))
+
+lista_num = [n1,n2,n3,n4,n5]
+print("Aqui está a lista na ordem: ", sorted(lista_num))
